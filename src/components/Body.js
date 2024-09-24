63ee19f0bec1a21f3872e7a4c410b3e23be2195a
Pooja <kulkarni.pooja@northeastern.edu>
2024-09-24 16:28:26 -0400

    Episode 6 updated

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,7 +4,9 @@ import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 const Body = () => {
     //Local state variable- super powerful variable
-    const [listOfRestaurants, setListOfRestaurant] = useState(resList);
+    const [listOfRestaurants, setListOfRestaurant] = useState([]);
+    const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+    const [searchText, setSearchText] = useState("");
    
     useEffect( ()=>{
         fetchData();
@@ -16,8 +18,9 @@ const Body = () => {
 
         const json = await data.json(); 
        // console.log(json);
-        //setListOfRestaurant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
-        console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+        setListOfRestaurant(json?.data?.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+        console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+        setFilteredRestaurant(json?.data?.cards[4].card.card.gridElements.infoWithStyle.restaurants);
     }; 
     //conditional rendering
     if(listOfRestaurants.length === 0){
@@ -30,11 +33,26 @@ const Body = () => {
     return(
         <div className="body">
             <div className="filter">
+                <div className="search">
+                    <input type="text" className="search-box" value={searchText} 
+                    onChange={(e)=>{
+                        setSearchText(e.target.value);
+                    }}/>
+                    <button onClick={()=>{
+                        //filter the restaurant cards and update UI
+                        //searchText
+                        //Value is bind to searchText and this searchText is empty so i cant type anything
+                        const filteredRestaurant = listOfRestaurants.filter((res)=>
+                            res.info.name.toLowerCase().includes(searchText.toLowerCase())
+                        );
+                        setFilteredRestaurant(filteredRestaurant);
+                    }}>Search</button>
+                </div>
                 <button 
                 className="filter-btn"
                 onClick={ ()=> {
                     const filteredList = listOfRestaurants.filter(
-                        (res) => res.data?.avgRating > 4
+                        (res) => res.info?.avgRating > 4
                     );
 
                     //console.log(filteredList);
@@ -43,12 +61,13 @@ const Body = () => {
                 >
                 Top Rated Restaurants  
                 </button>
+
             </div>
             <div className="res-container">
                 {
-                    listOfRestaurants.map((restaurant) => (
-                        <RestaurantCard key={restaurant.data.id} resData={restaurant}/>
-                    ))//key={restaurant.data.id}
+                    filteredRestaurant.map((restaurant) => (
+                        <RestaurantCard key={restaurant.info.id} resData={restaurant}/>
+                    ))
                 }
                 
                 
