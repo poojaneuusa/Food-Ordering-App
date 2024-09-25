35f067982c934ef6a6da54439b29be0e1409c07c
Pooja <kulkarni.pooja@northeastern.edu>
2024-09-24 21:23:09 -0400

    Added client side routing

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+const About = () => {
+    return(
+        <div>
+            <h1>About Us</h1>
+            <h2>This is Food ordering app built using React!</h2>
+        </div>
+    )
+}
+export default About;
